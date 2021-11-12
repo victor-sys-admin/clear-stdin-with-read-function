706eb8cb24762861a8fccd6fc666dc6fc5ce845e
Vik <93325630+victor-sys-admin@users.noreply.github.com>
2021-11-12 13:49:57 +0100

    Update test.c
    An improved version of test.c

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 /*
 ** Victor, 2021
-** test.c
+** basniv1.c
 ** File description:
 ** hello $name (low level)
 */
@@ -10,32 +10,43 @@
 int main() {
     char name[10];
     int i = 0;
+    char buffername = 0;
 
+    for(int i =0; i <= 9; i++) {
+        name[i] = 0;
+    }
+ 
     write(1, "Enter your name (Max 10 char) : ", 33);
-
-    read(0, &name[i], 1);
-    i++;
-    while(name[i-1] != '\n' && i < 10) {
-        read(0, &name[i], 1);
+    do {
+        if(i <= 9) {
+            read(0, &name[i], 1);
+            buffername = name[i];
+            if(buffername <= '`' && buffername >= '[' || buffername < 'A' || buffername > 'z' || buffername != ' ' && buffername != '\n') {
+                write(2, "\tError :\n\tThis does not look like a name\n", 42);
+                clear_stdin();
+                return 84;
+            }
+            if(buffername < 'A' || buffername > 'z') {
+                if(buffername != ' ' && buffername != '\n') {
+                    write(2, "\tError :\n\tThis does not look like a name\n", 42);
+                    clear_stdin();
+                    return 84;
+                }
+            }
+        }
         i++;
-    }
-    
-    if(i == 10) {
+    } while(name[i-1] != '\n' && i < 11);
+
+    if(i == 11) {
         clear_stdin();
-        write(1, "Hello ", 6);
-        write(1, name, 10);
-        write(1, "\n", 1);
-        return 0;
-    }
-    
-    name[i-1] = 0;
-    if(name[0] == 0) {
+    } else if(i == 1) {
         write(2, "\tError :\n\tEmpty name !\n", 24);
         return 84;
     } else {
-        write(1, "Hello ", 6);
-        write(1, name, 10);
-        write(1, "\n", 1);
-        return 0;
+        name[i-1] = 0;
     }
+
+    write(1, "Hello ", 6);
+    write(1, name, 10);
+    write(1, "\n", 1);
 }
